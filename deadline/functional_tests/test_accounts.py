fc26886485bd0b2045e6868698769352b19c2e35
Enether <familyguyuser192@windowslive.com>
2017-02-18 23:54:56 +0200

    Added a test for the accounts app's register view

--- deadline/functional_tests/test_accounts.py
+++ deadline/functional_tests/test_accounts.py
@@ -1,18 +1,29 @@
 import re
 
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.shortcuts import reverse
 from django.http import HttpResponse
-from django.core import mail
+
+from accounts.models import User
 
 
-class TestAccounts(TestCase):
+class TestAccounts(APITestCase):
     def test_register(self):
         # The user posts his username, email and password to the /accounts/register URL
-        email_address = 'meredith@abv.bg'
-        response: HttpResponse = self.client.post('/accounts/register', data={'username': 'Meredith',
-                                                  'password': 'mer192',
-                                                  'email': email_address})
-
+        response: HttpResponse = self.client.post('/accounts/register/', data={'username': 'Meredith',
+                                                  'password': 'mer19222',
+                                                  'email': 'meredith@abv.bg'})
         # Should have successfully register the user and gave him a user token
         self.assertEqual(response.status_code, 201)
-        self.assertTrue(hasattr(response, 'user_token'))
+        self.assertTrue('user_token' in response.data)
+
+    def test_register_existing_user_should_return_400(self):
+        User.objects.create(email='that_part@abv.bg', password='123', username='ThatPart')
+
+        response: HttpResponse = self.client.post('/accounts/register/', data={'username': 'Meredith',
+                                                                               'password': 'mer19222',
+                                                                               'email': 'that_part@abv.bg'})
+        # Should have successfully register the user and gave him a user token
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data)
+        self.assertIn('email already exists', ''.join(response.data['email']))
