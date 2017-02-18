7bb0b4daa716a875d54186b9c499969ac67327f7
Enether <familyguyuser192@windowslive.com>
2017-02-19 00:39:22 +0200

    Moved functional tests to unit tests

--- deadline/accounts/tests.py
+++ deadline/accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils.six import BytesIO
+from rest_framework.test import APITestCase
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
@@ -37,3 +38,36 @@ class UserModelTest(TestCase):
         self.assertEqual(deser_user.email, 'me@abv.bg')
         self.assertNotEqual(deser_user.password, '123')  # should be hashed!
         self.assertEqual(deser_user.score, 123)
+
+
+class RegisterViewTest(APITestCase):
+    def test_register(self):
+        # The user posts his username, email and password to the /accounts/register URL
+        response: HttpResponse = self.client.post('/accounts/register/', data={'username': 'Meredith',
+                                                                               'password': 'mer19222',
+                                                                               'email': 'meredith@abv.bg'})
+        # Should have successfully register the user and gave him a user token
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue('user_token' in response.data)
+
+    def test_register_existing_user_should_return_400(self):
+        User.objects.create(email='that_part@abv.bg', password='123', username='ThatPart')
+
+        response: HttpResponse = self.client.post('/accounts/register/', data={'username': 'Meredith',
+                                                                               'password': 'mer19222',
+                                                                               'email': 'that_part@abv.bg'})
+        # Should have successfully register the user and gave him a user token
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data)
+        self.assertIn('email already exists', ''.join(response.data['email']))
+
+class LoginViewTest(APITestCase):
+    def test_logging_in_the_system(self):
+        # There is a user account
+        User.objects.create(email='that_part@abv.bg', password='123')
+        # And we try logging in to it
+        response: HttpResponse = self.client.post('/accounts/login/', data={'email': 'that_part@abv.bg',
+                                                                            'password': '123'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('user_token' in response.data)
---
+++ deadline/functional_tests/functional_tests.py
@@ -0,0 +1,1 @@
+# TODO: Add functional tests
