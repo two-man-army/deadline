597ae26948559a3fa9eb3f776bbad77a37a108cb
nemalp <lazarov.pl@gmail.com>
2017-02-19 21:45:25 +0200

    Add basic authorization

---
+++ angular/app/auth/auth.controller.js
@@ -0,0 +1,56 @@
+(function() {
+    'use strict'
+
+    angular.module('app.auth')
+        .controller('AuthController', AuthController)
+        .run(run);
+
+    AuthController.$inject = ['$http', 'authService', '$location']
+
+    function AuthController($http, authService, $location) {
+        var vm = this;
+
+        vm.register = register
+        vm.login = login
+        vm.get_csrf_token = get_csrf_token
+
+        function register(user) {
+
+        }
+
+        function login($event, user) {
+            $event.preventDefault()
+
+            authService.login(user)
+                .then(
+                    function(res) {
+                        debugger;
+                        sessionStorage['authToken'] = response.data.user_tocken;
+                        $location.path('/dashboard');
+                    },
+
+                    function(error) {
+                        console.log($http.defaults.headers)
+                    }
+                );
+        }
+
+        function get_csrf_token() {
+            authService.get_csrf_token()
+                .then(
+                    function(response) {
+                        console.log(response)
+                    },
+
+                    function(error) {
+                        console.log(error)
+                    })
+        }
+    }
+
+    function run($http) {
+        $http.defaults.headers.post['X-CSRFToken'] = 'Ia57OLegUnsCqEie9kmmZuHfxNUqqunUYD5yLD3Ug2RSJNSEq8sUTQJcDcrcDvs6';
+        $http.defaults.headers.common.Authorization = 'Token Ia57OLegUnsCqEie9kmmZuHfxNUqqunUYD5yLD3Ug2RSJNSEq8sUTQJcDcrcDvs6';
+    }
+
+}())
