901f69a75209262db3d9056bd55f3674acaad3f3
Enether <familyguyuser192@windowslive.com>
2017-02-21 14:23:31 +0200

    Added a helper to update the user's score after a submission has been rated

--- deadline/challenges/helper.py
+++ deadline/challenges/helper.py
@@ -1,5 +1,6 @@
+from django.db.models import Max
 from challenges.models import Challenge, Submission, TestCase
-
+from accounts.models import User
 
 def grade_result(submission: Submission):
     """ Given a tested submission, update it's score in accordance to the number of test cases passed"""
@@ -10,3 +11,27 @@ def grade_result(submission: Submission):
 
     submission.result_score = challenge.score / num_successful_tests
     submission.save()
+
+
+def update_user_score(user: User, submission: Submission) -> bool:
+    """
+    Given a tested submission with score, update the user's score IF:
+        1. He has not submitted for the given challenge before
+        2. He has submitted for the given challenge before but with a lower score
+    Returns a boolean whether the score was updated or not
+    """
+    prev_submission = Submission.objects.filter(challenge=submission.challenge, author=user).all().aggregate(Max('result_score'))
+    if prev_submission is not None and prev_submission.result_score < submission.result_score:
+        # The user has submitted a better-scoring solution. Update his score
+        score_improvement = submission.result_score - prev_submission.result_score
+        user.score += score_improvement
+        user.save()
+        return True
+    elif prev_submission is None:
+        # The user does not have any previous submissions, so we update his score
+        user.score += submission.result_score
+        user.save()
+        return True
+
+    return False
+
--- deadline/challenges/tests/test_helpers.py
+++ deadline/challenges/tests/test_helpers.py
@@ -2,7 +2,7 @@ import random
 from django.test import TestCase
 from challenges.models import Challenge, Submission, TestCase
 from accounts.models import User
-from challenges.helper import grade_result
+from challenges.helper import grade_result, update_user_score
 
 
 class GradeResultTests(TestCase):
@@ -35,3 +35,58 @@ class GradeResultTests(TestCase):
 
         # Should have updated the submission's score
         self.assertEqual(self.submission.result_score, expected_score)
+
+
+class UpdateUserScoreTests(TestCase):
+    """
+    the update_user_score function should update the user's overall score given a submission by him.
+    Note: It should only update it if the user has a previous submission (or none at all) which has a lower score.
+    """
+    def setUp(self):
+        self.user = User(email="hello@abv.bg", password='123', username='me')
+        self.user.save()
+        self.challenge = Challenge(name='Hello World!', description='Say hello',
+                                   rating=10, score=100, test_file_name='smth',
+                                   test_case_count=3)
+        self.challenge.save()
+        self.submission = Submission(challenge=self.challenge, author=self.user,
+                                     code='hack you', task_id='123', result_score=20)
+        self.submission.save()
+        # add this submisssions score to him
+        self.user.score = 20
+        self.user.save()
+
+    def test_submission_with_lower_score_should_not_update(self):
+        lower_submission = Submission(challenge=self.challenge, author=self.user,
+                                     code='hack you', task_id='123', result_score=10)
+        lower_submission.save()
+
+        result = update_user_score(user=self.user, submission=lower_submission)
+        self.assertFalse(result)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user, 20)  # should not have changed
+
+    def test_submission_with_higher_score_should_update(self):
+        higher_submission = Submission(challenge=self.challenge, author=self.user,
+                                       code='hack you', task_id='123', result_score=30)
+        higher_submission.save()
+
+        result = update_user_score(user=self.user, submission=higher_submission)
+        self.assertTrue(result)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user, 30)  # should have changed
+
+    def test_submission_other_challenge_should_update(self):
+        """ Since the user does not have a submission for this challenge, it should update his score """
+        new_challenge = Challenge(name='NEW MAN', description='NEW',
+                                   rating=10, score=100, test_file_name='smth',
+                                   test_case_count=3)
+        new_challenge.save()
+        new_submission = Submission(challenge=new_challenge, author=self.user,
+                                      code='hack you', task_id='123', result_score=100)
+        new_submission.save()
+
+        result = update_user_score(user=self.user, submission=new_challenge)
+        self.assertTrue(result)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user, 120)  # should have updated it
