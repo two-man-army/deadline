87fc119f0a358978a2db2879d5324fca9e95e929
nemalp <lazarov.pl@gmail.com>
2017-02-21 09:43:24 +0000

    Add app routes and restrict unauthorized user to access given route

--- angular/app/app.module.js
+++ angular/app/app.module.js
@@ -7,16 +7,41 @@
         'app.layout',
         'app.auth',
         'app.dashboard'
-        ])
+    ])
         .config(config)
+        .run(run)
         .constant('BASE_URL', 'http://localhost:8000/');
 
     config.$inject = ['$routeProvider'];
+    run.$inject = ['$rootScope', 'authService']
 
     function config($routeProvider) {
-        $routeProvider.otherwise({
-            redirectTo: '/'
-        });
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/layout/templates/layout.html',
+                controllerAs: 'vm',
+                controller: 'LayoutVideoController',
+                requireLogin: false
+            })
+
+            .when('/dashboard', {
+                templateUrl: 'app/dashboard/templates/dashboard.html',
+                controllerAs: 'vm',
+                controller: 'DashboardController',
+                requireLogin: true
+            })
+
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+
+    function run($rootScope, authService) {
+        $rootScope.$on('$routeChangeStart', function($event, next, current) {
+            if(next.requireLogin && !authService.isAuthenticated()) {
+                $event.preventDefault();
+            }
+        })
     }
 }());
 
