b0545188d7f97a3daa9c5de5d97152b0ffd9602b
Enether <familyguyuser192@windowslive.com>
2017-03-15 17:12:31 +0200

    Fixed the RustGrader. It now works properly-ish
    It used to incorrectly judge if a solution would compile
    It used to not send input to the process

--- deadline/challenges/grader.py
+++ deadline/challenges/grader.py
@@ -37,9 +37,9 @@ class RustGrader:
 
     def run_solution(self):
         self.create_solution_file()
-        sorted_files = self.find_tests()
+        sorted_input_files, sorted_output_files = self.find_tests()
 
-        self.read_tests(sorted_files)
+        self.read_tests(sorted_input_files, sorted_output_files)
         self.compile()
         self.delete_solution_file(self.temp_file_name)
 
@@ -57,7 +57,7 @@ class RustGrader:
         compile_result = compiler_proc.communicate()
         compiler_proc.kill()
         error_message = compile_result[1].decode()
-        if error_message:
+        if error_message and 'error: aborting due to previous error' in error_message:
             # There is an error while compiling
             self.compiled = False
             # update the solution
@@ -80,9 +80,9 @@ class RustGrader:
         # TODO: Docker
         # TODO: Timer
         program_process = subprocess.Popen(['/home/netherblood/PycharmProjects/two-man-army/deadline/deadline/'
-                                            + self.temp_exe_file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                            + self.temp_exe_file_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # Enter the input
-        results = program_process.communicate(input='\n'.join(test_case.input_lines))
+        results = program_process.communicate(input='\n'.join(test_case.input_lines).encode())
 
         result_dict = {
             "error_message": "",
@@ -115,28 +115,31 @@ class RustGrader:
         self.tests_folder: str = os.path.join(TESTS_LOCATION, self.challenge.test_file_name)
         if not os.path.isdir(self.tests_folder):
             raise Exception(f'The path {self.tests_folder} is invalid!')
-        files = []
 
         # Read  the files in the directory
+        input_files, output_files = [], []
         for file in os.scandir(self.tests_folder):
-            if file.name.startswith('input') or file.name.startswith('output'):
-                files.append(file)
+            if file.name.startswith('input'):
+                input_files.append(file)
+            elif file.name.startswith('output'):
+                output_files.append(file)
 
         # There must be two files for every test case
-        if len(files) != (2*self.challenge.test_case_count) or len(files) % 2 != 0:
+        files_len = len(input_files) + len(output_files)
+        if files_len != (2*self.challenge.test_case_count) or files_len % 2 != 0:
             raise Exception('Invalid input/output file count!')
 
-        return list(sorted(files, key=lambda file: file.name))
+        return list(sorted(input_files, key=lambda file: file.name)), list(sorted(output_files, key=lambda file: file.name))
 
-    def read_tests(self, sorted_files):
+    def read_tests(self, sorted_input_files, sorted_output_files):
         idx = 0
 
-        while idx < len(sorted_files):
+        while idx < len(sorted_input_files):
             """ Since the files are sorted by name, an input file should be followed by an output file
                 i.e  input-01.txt output-01.txt input-02.txt output-02.txt """
             input_lines, output_lines = [], []
-            input_file = sorted_files[idx]
-            output_file = sorted_files[idx + 1]
+            input_file = sorted_input_files[idx]
+            output_file = sorted_output_files[idx]
 
             if 'input' not in input_file.name or 'output' not in output_file.name:
                 raise Exception('Invalid input/output file names when reading them.')
@@ -146,7 +149,7 @@ class RustGrader:
             with open(os.path.abspath(output_file.path)) as f:
                 output_lines = [line.strip() for line in f.readlines()]
 
-            idx += 2
+            idx += 1
 
             self.test_cases.append(RustTestCase(input_lines=input_lines, expected_output_lines=output_lines))
 
--- deadline/challenges/tests/test_grader.py
+++ deadline/challenges/tests/test_grader.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 from challenges.grader import RustGrader, RustTestCase
 from unittest.mock import MagicMock
 
-
+# TODO: Test will need rework after the timing of the test is functional, since the hardcoded expected JSONs
+# have "time": "0s" in it and there will be no way to know the amount of time it'll take to run the program
 class RustGraderTest(TestCase):
     def test_grader_should_not_compile(self):
         """ It should take a Challenge object and find its tests in the appropriate folder"""
@@ -56,3 +57,42 @@ class RustGraderTest(TestCase):
 
         self.assertEqual(json_result, expected_json)
 
+    def test_grader_sum_array(self):
+        challenge = MagicMock(test_case_count=5, test_file_name='array_sum_tests')
+        challenge.name = 'Sum Array'
+        sol = MagicMock()
+        sol.code = """use std::io;
+
+fn main(){
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut input);
+    let numbers: Vec<f64> = input.trim().split(", ").map(|x| x.parse::<f64>().unwrap()).collect();
+    let sum: f64 = numbers.iter().sum();
+    println!("{}", sum);
+}
+"""
+        expected_json = '{"results": [{"error_message": "", "success": true, "time": "0s"}, {"error_message": "", "success": true, "time": "0s"}, {"error_message": "", "success": true, "time": "0s"}, {"error_message": "", "success": true, "time": "0s"}, {"error_message": "", "success": true, "time": "0s"}]}'
+        rg = RustGrader(challenge, sol)
+
+        self.assertEqual(rg.run_solution(), expected_json)
+
+    def test_grader_sum_array_incorrect(self):
+        challenge = MagicMock(test_case_count=5, test_file_name='array_sum_tests')
+        challenge.name = 'Sum Array'
+        sol = MagicMock()
+        sol.code = """use std::io;
+
+        fn main(){
+            let mut input = String::new();
+            let stdin = std::io::stdin();
+            stdin.read_line(&mut input);
+            let numbers: Vec<f64> = input.trim().split(", ").map(|x| x.parse::<f64>().unwrap()).collect();
+            let sum: f64 = numbers.iter().sum();
+            println!("{}", sum+1.0);
+        }
+
+        """
+        expected_json = '{"results": [{"error_message": "16 is not equal to the expected 15", "success": false, "time": "0s"}, {"error_message": "91 is not equal to the expected 90", "success": false, "time": "0s"}, {"error_message": "34 is not equal to the expected 33", "success": false, "time": "0s"}, {"error_message": "1 is not equal to the expected 0", "success": false, "time": "0s"}, {"error_message": "4594850 is not equal to the expected 4594849", "success": false, "time": "0s"}]}'
+        rg = RustGrader(challenge, sol)
+        self.assertEqual(rg.run_solution(), expected_json)
