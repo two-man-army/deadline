22183ef51be9d2c97023dbb37c956e1c824d1f9c
Enether <familyguyuser192@windowslive.com>
2017-03-20 16:30:30 +0200

    Migrated all challenges to use input/output text files

--- deadline/challenge_tests/hello_tests.py
+++
@@ -1,7 +0,0 @@
-from grader import *
-
-
-@test_cases(['Hello World'], description='Searching for word {0}')
-def search(m, wanted_str):
-    output = m.stdout.new()
-    assert wanted_str in output, '{0} not in {1}'.format(wanted_str, output)
