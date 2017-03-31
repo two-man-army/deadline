542b329979feabb0a06e041d8020a30e61fa4555
Enether <familyguyuser192@windowslive.com>
2017-03-31 11:12:18 +0300

    Slight changes in hope of getting Travis to work

--- deadline/manage.py
+++ deadline/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deadline.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deadline.settings.local")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
