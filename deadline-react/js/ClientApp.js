e1c36a4e759b9337defad25012dca4db385230b4
nemalp <lazarov.pl@gmail.com>
2017-04-30 20:44:49 +0300

    Regactor component

--- deadline-react/js/ClientApp.js
+++ deadline-react/js/ClientApp.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { render } from 'react-dom'
+import LoginPage from './LoginPage'
 
 const selector = document.getElementById('app')
 
 const App = (props) => {
   return (
     <div className='app'>
-      <div className='landing'>
-        <div>Hello0, {props.name}!</div>
-      </div>
+      <LoginPage />
     </div>
   )
 }
@@ -18,4 +17,4 @@ App.propTypes = {
   name: PropTypes.string
 }
 
-render(<App name='c0re' />, selector)
+render(<App name='deadline' />, selector)
