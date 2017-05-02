496f551d5ec3079734d595b494f8e52f206b1d12
nemalp <lazarov.pl@gmail.com>
2017-05-02 23:03:32 +0300

    Refactor RegisterForm

--- deadline-react/js/RegisterForm.js
+++ deadline-react/js/RegisterForm.js
@@ -1,69 +1,81 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const RegisterForm = ({onSubmit, onChange}) => (
-  <form onSubmit={onSubmit}>
-    <div className='sign-up-htm'>
-      <div className='group'>
-        <label htmlFor='username' className='label'>Username</label>
-        <input
-          id='username'
-          type='text'
-          name='username'
-          className='input'
-          onChange={onChange}
+const RegisterForm = ({onSubmit, onChange, repeatedPasswordIsInvalid}) => {
+  let repeatedPasswordStyle = {}
+
+  if (repeatedPasswordIsInvalid) {
+    repeatedPasswordStyle = {
+      boxShadow: '0 0 20px red'
+    }
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className='sign-up-htm'>
+        <div className='group'>
+          <label htmlFor='username' className='label'>Username</label>
+          <input
+            id='username'
+            type='text'
+            name='username'
+            className='input'
+            onChange={onChange}
         />
-      </div>
-      <div className='group'>
-        <label htmlFor='reg-pass' className='label'>Password</label>
-        <input
-          id='reg-pass'
-          type='password'
-          name='password'
-          className='input'
-          data-type='password'
-          onChange={onChange}
+        </div>
+        <div className='group'>
+          <label htmlFor='reg-pass' className='label'>Password</label>
+          <input
+            id='reg-pass'
+            type='password'
+            name='password'
+            className='input'
+            data-type='password'
+            onChange={onChange}
         />
-      </div>
-      <div className='group'>
-        <label htmlFor='repeat-pass' className='label'>Repeat Password</label>
-        <input
-          id='repeat-pass'
-          type='password'
-          name='repeatedPassword'
-          className='input'
-          data-type='password'
-          onChange={onChange}
+        </div>
+        <div className='group'>
+          <label htmlFor='repeat-pass' className='label'>Repeat Password</label>
+          <input
+            id='repeat-pass'
+            type='password'
+            name='repeatedPassword'
+            className='input'
+            data-type='password'
+            onChange={onChange}
+            style={repeatedPasswordStyle}
         />
-      </div>
-      <div className='group'>
-        <label htmlFor='reg-email' className='label'>Email Address</label>
-        <input
-          id='reg-email'
-          type='text'
-          name='email'
-          className='input'
-          onChange={onChange}
+        </div>
+        <div className='group'>
+          <label htmlFor='reg-email' className='label'>Email Address</label>
+          <input
+            id='reg-email'
+            type='text'
+            name='email'
+            className='input'
+            onChange={onChange}
         />
-      </div>
-      <div className='group'>
-        <input
-          type='submit'
-          className='button'
-          value='Sign Up'
+        </div>
+        <div className='group'>
+          <input
+            type='submit'
+            className='button'
+            value='Sign Up'
         />
+        </div>
+        <div className='hr' />
+        <div className='foot-lnk'>
+          <label htmlFor='tab-1'>Already Member?</label>
+        </div>
       </div>
-      <div className='hr' />
-      <div className='foot-lnk'>
-        <label htmlFor='tab-1'>Already Member?</label>
-      </div>
-    </div>
-  </form>
-)
+    </form>
+  )
+}
 
 RegisterForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
+  repeatedPasswordIsInvalid: PropTypes.bool.isRequired
 }
 
 export default RegisterForm
