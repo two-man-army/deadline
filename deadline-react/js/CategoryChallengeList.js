59ce3276cea4a5f5a4c6130e259d984f276ab900
Enether <familyguyuser192@windowslive.com>
2017-05-05 21:05:37 +0300

    Added a CategoryChallengeList component, listing challenges from a certain category

---
+++ deadline-react/js/CategoryChallengeList.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {getSubCategoryChallenges} from './requests.js'
+import ChallengeMetaInfo from './ChallengeMetaInfo.js'
+
+class CategoryChallengeList extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      challenges: []
+    }
+
+    this.subCategory = props.match.params.category
+
+    this.loadSubCategoryChallenges = this.loadSubCategoryChallenges.bind(this)
+    this.loadSubCategoryChallenges()
+  }
+
+  loadSubCategoryChallenges () {
+    console.log('Querying with ' + this.subCategory)
+    getSubCategoryChallenges(this.subCategory).then(subCat => {
+      let challenges = subCat.challenges
+      this.setState({challenges})
+    }).catch(err => {
+      // TODO: Handle error
+      throw err
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{this.subCategory} Challenges</h1>
+        {this.state.challenges.map(challenge => {
+          return <ChallengeMetaInfo {...challenge} />
+        })}
+      </div>
+    )
+  }
+}
+
+CategoryChallengeList.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      category: PropTypes.string.isRequired
+    })
+  })
+}
+export default CategoryChallengeList
