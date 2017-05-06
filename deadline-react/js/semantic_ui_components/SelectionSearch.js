7f5fd154aff8e2156fa560b6f9189db2fd137567
nemalp <lazarov.pl@gmail.com>
2017-05-06 19:58:20 +0300

    Add propTypes validation

--- deadline-react/js/semantic_ui_components/SelectionSearch.js
+++ deadline-react/js/semantic_ui_components/SelectionSearch.js
@@ -1,5 +1,6 @@
 /** A Select component with icons and seach functionality */
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Dropdown } from 'semantic-ui-react'
 
 // options = [ { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan',  image: { avatar: true, src: '/assets/images/avatar/small/jenny.jpg' } }, ...  ]
@@ -7,5 +8,11 @@ import { Dropdown } from 'semantic-ui-react'
 const SelectionSearch = ({options, placeholder, onChange}) => (
   <Dropdown placeholder={placeholder} fluid search selection options={options} onChange={onChange} />
 )
+
+SelectionSearch.propTypes = {
+  options: PropTypes.func,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
+}
 
 export default SelectionSearch
