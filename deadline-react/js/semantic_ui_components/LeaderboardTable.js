b23d7059de5e1438233c20b526c4ad3bdac447cd
Enether <familyguyuser192@windowslive.com>
2017-05-07 17:52:18 +0300

    Added the ability to filter a Challenge Leaderboard by author/language

--- deadline-react/js/semantic_ui_components/LeaderboardTable.js
+++ deadline-react/js/semantic_ui_components/LeaderboardTable.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Icon, Header, Image, Table } from 'semantic-ui-react'
+import { Dropdown, Input, Icon, Header, Image, Table } from 'semantic-ui-react'
 import TimeAgo from 'react-timeago'
 
 class SubmissionsTable extends React.Component {
   constructor (props) {
     super(props)
 
+    this.state = {
+      submissions: this.props.submissions,
+      chosenSearchField: 'user',
+      searchQuery: ''
+    }
+
+    const searchOptions = [
+      { key: 'user', text: 'Author', value: 'user' },
+      { key: 'language', text: 'Language', value: 'language' }
+    ]
+
+    this.searchOptions = searchOptions
     this.buildSubmissionRow = this.buildSubmissionRow.bind(this)
+    this.filterSubmissionsBySearchQuery = this.filterSubmissionsBySearchQuery.bind(this)
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
   }
   buildSubmissionRow (submission) {
     let submissionStatusIcon = null
@@ -56,25 +71,79 @@ class SubmissionsTable extends React.Component {
     )
   }
 
+  handleSearch (ev, e) {
+    let searchVal = e.value
+    if (searchVal === '') {
+      this.setState({submissions: this.props.submissions})
+      return
+    }
+
+    let submissions = this.filterSubmissionsBySearchQuery(searchVal, this.state.chosenSearchField)
+    this.setState({submissions, searchQuery: e.value})
+  }
+
+  /**
+   * Returns the submissions whose {searchField} starts with the given {searchVal}
+   * i.e searchField = 'user', searchVal = 'mar' - will return only submissions from users whose author starts with Mar
+   * @param {String} searchVal
+   * @param {String} searchField
+   */
+  filterSubmissionsBySearchQuery (searchVal, searchField) {
+    searchVal = searchVal.toLowerCase()
+    searchField = searchField.toLowerCase()
+
+    if (searchField === 'user') {  // filter by author
+      return this.props.submissions.filter(submission => {
+        return submission.author.toLowerCase().startsWith(searchVal)
+      })
+    } else if (searchField === 'language') {  // filter by language
+      return this.props.submissions.filter(submission => {
+        return submission.language.toLowerCase().startsWith(searchVal)
+      })
+    } else {
+      return this.props.submissions
+    }
+  }
+
+  /**
+   * Handles the search term we're searching by change
+   * i.e Author -> Language (we want to search by language now)
+   * So re-query the search text for the different term
+   */
+  handleSearchTermChange (ev, e) {
+    let submissions = this.filterSubmissionsBySearchQuery(this.state.searchQuery, e.value)
+    this.setState({submissions, chosenSearchField: e.value})
+  }
+
   render () {
     return (
-      <Table basic='very' style={{margin: '20px auto', padding: '0 20px'}} >
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Author</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-            <Table.HeaderCell>Score</Table.HeaderCell>
-            <Table.HeaderCell>Language</Table.HeaderCell>
-            <Table.HeaderCell>Submitted</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+      <div>
+        <Input
+          action={<Dropdown basic floating options={this.searchOptions} defaultValue='user' onChange={this.handleSearchTermChange} />}
+          icon='search'
+          iconPosition='left'
+          placeholder='Search...'
+          onChange={this.handleSearch}
+          className='leaderboard-search'
+      />
+        <Table basic='very' style={{margin: '20px auto', padding: '0 20px'}} >
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Author</Table.HeaderCell>
+              <Table.HeaderCell>Status</Table.HeaderCell>
+              <Table.HeaderCell>Score</Table.HeaderCell>
+              <Table.HeaderCell>Language</Table.HeaderCell>
+              <Table.HeaderCell>Submitted</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-        <Table.Body>
-          {this.props.submissions.map(submission => {
-            return this.buildSubmissionRow(submission)
-          })}
-        </Table.Body>
-      </Table>
+          <Table.Body>
+            {this.state.submissions.map(submission => {
+              return this.buildSubmissionRow(submission)
+            })}
+          </Table.Body>
+        </Table>
+      </div>
     )
   }
 }
