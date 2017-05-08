0d40feebd69486ce1a7cae02c2ff6a6a3e1f45cf
nemalp <lazarov.pl@gmail.com>
2017-05-08 23:50:41 +0300

    Rewrite component

--- deadline-react/js/semantic_ui_components/OptionsList.js
+++ deadline-react/js/semantic_ui_components/OptionsList.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { List } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const OptionsList = () => (
-  <List link>
-    <List.Item as='a'>Edit Profile</List.Item>
-    <List.Item as='a'>Change Password</List.Item>
-    <List.Item as='a'>Log Out</List.Item>
-    <List.Item as='a'>Cancel</List.Item>
-  </List>
+  <div className='account-options'>
+    <ul>
+      <li><Link to='/accounts/password/change/'>Change Password</Link></li>
+      <li><Link to='/accounts/edit/'>Edit Profile</Link></li>
+      <li><Link to='/'>Something else</Link></li>
+      <li><Link to='/accounts/logout/'>Logout</Link></li>
+      <li><Link to='/'>Something else</Link></li>
+    </ul>
+  </div>
 )
 
 export default OptionsList
