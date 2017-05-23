d08aa0ab7f7ad9758c2457b39accc1ebcd46a37e
nemalp <lazarov.pl@gmail.com>
2017-05-23 22:58:18 +0300

    Refactor ProfileOptions

--- deadline-react/js/semantic_ui_components/ProfileOptions.js
+++ deadline-react/js/semantic_ui_components/ProfileOptions.js
@@ -1,23 +1,53 @@
 import React from 'react'
-import { Button, Modal, Image, Header } from 'semantic-ui-react'
+import { Button, Modal, Image } from 'semantic-ui-react'
 import OptionsList from './OptionsList'
 
-const ProfileOptions = () => (
-  <Modal
-    dimmer='blurring'
-    trigger={<img src='/assets/img/avatar.jpg' />}>
-    <Modal.Header>Select a Photo</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='/assets/img/avatar.jpg' />
-      <Modal.Description>
-        <Header>Draft. Additional styles and logic should be applied. Probably material ui List will be more handy</Header>
-        <OptionsList />
-      </Modal.Description>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button positive icon='checkmark' labelPosition='right' content='Cancel/Go?' />
-    </Modal.Actions>
-  </Modal>
-)
+class ProfileOptions extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      open: false
+    }
+
+    this.handleClose = this.handleClose.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleOpen () {
+    this.setState({open: true})
+  }
+
+  handleClose () {
+    this.setState({open: false})
+  }
+
+  handleClick () {
+    this.setState({open: false})
+  }
+
+  render () {
+    return (
+      <div>
+        <img src='/assets/img/avatar.jpg' onClick={this.handleOpen} />
+        <Modal
+          dimmer='blurring'
+          open={this.state.open}>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+            <Image wrapped size='medium' src='/assets/img/avatar.jpg' />
+            <Modal.Description className='account-dialog'>
+              <OptionsList onClick={this.handleClick} />
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button secondary content='Cancel' onClick={this.handleClose} />
+          </Modal.Actions>
+        </Modal>
+      </div>
+    )
+  }
+}
 
 export default ProfileOptions
