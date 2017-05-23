894e286552d338272ded159feb77e955b76bb197
nemalp <lazarov.pl@gmail.com>
2017-05-23 22:57:25 +0300

    Refactor OptionsList

--- deadline-react/js/semantic_ui_components/OptionsList.js
+++ deadline-react/js/semantic_ui_components/OptionsList.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { List } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const OptionsList = () => (
-  <List link>
-    <List.Item as='a'>Edit Profile</List.Item>
-    <List.Item as='a'>Change Password</List.Item>
-    <List.Item as='a'>Log Out</List.Item>
-    <List.Item as='a'>Cancel</List.Item>
-  </List>
+const OptionsList = ({onClick}) => (
+  <div className='account-options'>
+    <ul>
+      <li><Link to='/accounts/password/change/' onClick={onClick}>Change Password</Link></li>
+      <li><Link to='/accounts/edit/' onClick={onClick}>Edit Profile</Link></li>
+      <li><Link to='/accounts/something'>Something else</Link></li>
+      <li><Link to='/accounts/logout/'>Logout</Link></li>
+      <li><Link to='/accounts/else'>Something else</Link></li>
+    </ul>
+  </div>
 )
+
+OptionsList.propTypes = {
+  onClick: PropTypes.func.isRequired
+}
 
 export default OptionsList
