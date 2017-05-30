95f5bcad7ce8b4ec0302a3c6ee7836029a3468fa
Enether <familyguyuser192@windowslive.com>
2017-05-30 16:23:41 +0300

    Removed debug lines and fixed the faulty, CI-failing SQL query

--- deadline_/sql_queries.py
+++ deadline_/sql_queries.py
@@ -4,19 +4,20 @@ SUBMISSION_SELECT_TOP_SUBMISSIONS_FOR_CHALLENGE = ('SELECT id, author_id, max(re
                                                    'WHERE challenge_id = %s '
                                                    'GROUP BY author_id '
                                                    'ORDER BY maxscore DESC, created_at ASC;')
+# Does not pass CI
 # SUBMISSION_SELECT_TOP_SUBMISSION_FOR_CHALLENGE_BY_USER = ('SELECT id, max(result_score) as maxscore '
 #                                                           'FROM challenges_submission '
 #                                                           'WHERE challenge_id = %s '
 #                                                           'AND author_id = %s '
 #                                                           'AND pending = 0')
-# SUBMISSION_SELECT_TOP_SUBMISSION_FOR_CHALLENGE_BY_USER = ('SELECT id, result_score as maxscore '
-#                                                           'FROM challenges_submission '
-#                                                           'WHERE challenge_id = %s '
-#                                                           'AND author_id = %s'
-#                                                           'AND pending = 0'
-#                                                           'ORDER BY result_score DESC'
-#                                                           'LIMIT 1;')
-SUBMISSION_SELECT_TOP_SUBMISSION_FOR_CHALLENGE_BY_USER = 'SELECT id, result_score as maxscore FROM challenges_submission WHERE challenge_id = %s and author_id = %s and pending = 0 ORDER by result_score DESC LIMIT 1;'
+SUBMISSION_SELECT_TOP_SUBMISSION_FOR_CHALLENGE_BY_USER = ('SELECT id, result_score as maxscore '
+                                                          'FROM challenges_submission '
+                                                          'WHERE challenge_id = %s AND '
+                                                          'author_id = %s AND '
+                                                          'pending = 0 '
+                                                          'ORDER by result_score DESC '
+                                                          'LIMIT 1;')
+
 SUBMISSION_SELECT_LAST_10_SUBMISSIONS_GROUPED_BY_CHALLENGE_BY_AUTHOR = ('SELECT * '
                                                                         'FROM challenges_submission '
                                                                         'WHERE author_id = %s '
