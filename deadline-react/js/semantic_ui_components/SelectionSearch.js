407e276e4e420842fd98479a0c6f4acb51ec1251
nemalp <lazarov.pl@gmail.com>
2017-06-22 23:38:25 +0300

    Add style prop to SelectionSearch

--- deadline-react/js/semantic_ui_components/SelectionSearch.js
+++ deadline-react/js/semantic_ui_components/SelectionSearch.js
@@ -5,11 +5,12 @@ import { Dropdown } from 'semantic-ui-react'
 
 // options = [ { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan',  image: { avatar: true, src: '/assets/images/avatar/small/jenny.jpg' } }, ...  ]
 
-const SelectionSearch = ({options, placeholder, onChange}) => (
-  <Dropdown placeholder={placeholder} fluid search selection options={options} onChange={onChange} />
+const SelectionSearch = ({options, placeholder, onChange, style}) => (
+  <Dropdown placeholder={placeholder} style={style} fluid search selection options={options} onChange={onChange} />
 )
 
 SelectionSearch.propTypes = {
+  style: PropTypes.string,
   options: PropTypes.array,
   placeholder: PropTypes.string,
   onChange: PropTypes.func
