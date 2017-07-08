241d86c1519b0131e3b011fdab65eb8b13b6badf
Enether <familyguyuser192@windowslive.com>
2017-07-08 21:03:41 +0300

    Added new permission class - IsTeacherOfCourse

--- deadline_/education/permissions.py
+++ deadline_/education/permissions.py
@@ -13,6 +13,19 @@ class IsTeacher(BasePermission):
         return request.user.role.name == TEACHER_ROLE_NAME
 
 
+class IsTeacherOfCourse(BasePermission):
+    """
+        Permissions that allows only people that are teachers of the associated Course
+        """
+    @staticmethod
+    def can_access(course, user):
+        return course.has_teacher(user)
+
+    def has_object_permission(self, request, view, obj):
+        course = obj if isinstance(obj, Course) else obj.get_course()
+        return self.can_access(course, request.user)
+
+
 class IsEnrolledOnCourseOrIsTeacher(BasePermission):
     """
     Permissions that allows only people that are allowed on the associated course (or are teachers of it)
