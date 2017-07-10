43ecf94cbd993be3533e01c5b27b6140913ce1a0
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-07-10 09:39:38 +0300

    Moved the fetch_models decorator and added tests for it

---
+++ deadline_/decorators.py
@@ -0,0 +1,51 @@
+"""
+Function/class decorators used across apps
+"""
+import collections
+from rest_framework.response import Response
+
+from helpers import fetch_models_by_pks
+
+
+def fetch_models(response_function, *args, **kwargs):
+    """
+    This is a decorator for a ClassView's response method (i.e GET, POST, etc).
+    It fetches the Model instances using the given primary keys in the URL.
+    Requires the ClassView to have defined:
+        model_classes: iterable - the Django ORM classes we'll fetch in the EXACT order that the PKs are in the URL
+            example: given the url /course/{course_id}/lesson/{lesson_id} - model_classes MUST be [Course, Lesson],
+                otherwise it might fetch the lesson via the course_id.
+    Optional in the ClassView:
+        main_class: a class - specifies the main class of the view. Given this, this decorator will check all
+            the permission_classes for a has_object_permissions method and call it.
+            This is done to check for permissions easily.
+             If it does not have the permission, the decorator outright returns a 403 Response
+    If a model fetch fails, it outright returns a 404 response with the given error message
+
+    If it succeeds, it sends the objects as parameters in the function
+    """
+    # TODO: Test, a lot
+    from errors import FetchError
+
+    def view_decorator(class_view, *args, **kwargs):
+        nonlocal response_function
+        request = args[0]
+        if not hasattr(class_view, 'model_classes') and not isinstance(class_view.model_classes, collections.Iterable):
+            raise Exception(f'Class {class_view} should have the model_classes iterable variable defined!')
+        if len(class_view.model_classes) != len(kwargs.keys()):
+            raise Exception(f'Class {class_view} does not have enough classes defined in the model_classes!')
+
+        try:
+            models = fetch_models_by_pks({model: model_pk for model, model_pk in zip(class_view.model_classes, kwargs.values())})
+            if hasattr(class_view, 'main_class') and hasattr(class_view, 'permission_classes'):
+                main_obj = [model for model in models if isinstance(model, class_view.main_class)][0]
+                for permission in [permission for permission in class_view.permission_classes
+                                   if hasattr(permission, 'has_object_permission')]:
+                    if not permission.has_object_permission(request, class_view, main_obj):
+                        return Response(status=403)
+
+            return response_function(class_view, request, *(list(models) + list(args)), **kwargs)
+        except FetchError as e:
+            return Response(status=404, data={'error': str(e)})
+
+    return view_decorator
--- deadline_/tests.py
+++ deadline_/tests.py
@@ -1,10 +1,13 @@
 from random import randint
+from unittest.mock import MagicMock
 
 from django.test import TestCase
+from rest_framework.response import Response
 
 from challenges.models import MainCategory, SubCategory
 from errors import FetchError
 from helpers import fetch_models_by_pks
+from decorators import fetch_models
 from challenges.tests.factories import MainCategoryFactory, SubCategoryFactory
 
 
@@ -43,3 +46,62 @@ class FetchModelsTest(TestCase):
             })
 
             self.assertEqual(expected_results, received_results)
+
+
+class FetchModelsDecoratorTest(TestCase):
+    def setUp(self):
+        class Tank:
+            model_classes = (MainCategory, SubCategory)
+            main_class = SubCategory
+
+            @fetch_models
+            def get(self, request, main_cat, sub_cat, *args, **kwargs):
+                return request, main_cat, sub_cat
+        self.Tank = Tank
+        self.main_cat = MainCategoryFactory()
+        self.sub_cat = SubCategoryFactory()
+
+    def test_attaches_models(self):
+        """ Also asserts that we send back the results from our function """
+        f_instance = self.Tank()
+        _, main_cat, sub_cat = f_instance.get(0, main_cat_pk=self.main_cat.id, subcat_pk=self.sub_cat.id)
+        self.assertEqual(self.main_cat, main_cat)
+        self.assertEqual(self.sub_cat, sub_cat)
+
+    def test_returns_404__response_on_unsuccessful_fetch(self):
+        f_instance = self.Tank()
+        response = f_instance.get(0, main_cat_pk=200, subcat_pk=self.sub_cat.id)
+        self.assertTrue(isinstance(response, Response))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['error'], 'MainCategory with ID 200 does not exist.')
+
+    def test_raises_exception_on_more_pks_sent_than_classes_defined(self):
+        """ As we have two model classes defined, sending three or one PKs should raise an exception"""
+        f_instance = self.Tank()
+        with self.assertRaises(Exception):
+            f_instance.get(0, main_cat_pk=self.main_cat.id, subcat_pk=self.sub_cat.id, tank_pk=3)
+        with self.assertRaises(Exception):
+            f_instance.get(0, main_cat_pk=self.main_cat.id, )
+
+    def test_raises_exception_if_model_classes_not_defined(self):
+        delattr(self.Tank, 'model_classes')
+        f_instance = self.Tank()
+
+        with self.assertRaises(Exception):
+            f_instance.get(0, main_cat_pk=self.main_cat.id, subcat_pk=self.sub_cat.id)
+
+    def test_raises_exception_if_model_classes_not_iterable(self):
+        self.Tank.model_classes = 2
+        f_instance = self.Tank()
+
+        with self.assertRaises(Exception):
+            f_instance.get(0, main_cat_pk=self.main_cat.id, subcat_pk=self.sub_cat.id)
+
+    def test_calls_permissions_and_returns_403_if_permission_returns_false(self):
+        # TODO: Might want to test that its called as we want it
+        self.Tank.permission_classes = (MagicMock(has_object_permission = lambda x, y, z: False), )
+        f_instance = self.Tank()
+
+        response = f_instance.get(0, main_cat_pk=self.main_cat.id, subcat_pk=self.sub_cat.id)
+        self.assertTrue(isinstance(response, Response))
+        self.assertEqual(response.status_code, 403)
