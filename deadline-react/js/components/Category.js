0420cb7ec7a97e95bad03a5961d63f096e68670b
nemalp <lazarov.pl@gmail.com>
2017-07-26 23:27:53 +0300

    Refactoring component

--- deadline-react/js/components/Category.js
+++ deadline-react/js/components/Category.js
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 const Category = ({name, url}) => (
-  <li>
+  <li className='category'>
     <Link to={{pathname: url}}>
       <section>
-        <h3>{name}</h3>
+        <h3 className='category-header'>{name}</h3>
+        <div className='category-info'>
+          <div className='progress-subcategories'>.</div>
+          <div className='progress-challenges'>.</div>
+        </div>
       </section>
     </Link>
   </li>
