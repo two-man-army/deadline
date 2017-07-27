6357d409613dafb981a62c08fc722863c765b8e4
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-07-27 16:28:08 +0300

    [PostgreSQL-DB] Fixed top submissions SQL query to work with postgre

--- deadline_/sql_queries.py
+++ deadline_/sql_queries.py
@@ -1,9 +1,15 @@
 # Selects the top submissions for a given challenge, always selecting the TOP scoring submission for each author
-SUBMISSION_SELECT_TOP_SUBMISSIONS_FOR_CHALLENGE = ('SELECT id, author_id, max(result_score) as maxscore '
-                                                   'FROM challenges_submission '
-                                                   'WHERE challenge_id = %s '
-                                                   'GROUP BY author_id '
-                                                   'ORDER BY maxscore DESC, created_at ASC;')
+SUBMISSION_SELECT_TOP_SUBMISSIONS_FOR_CHALLENGE = (
+    'SELECT id, author_id, result_score '
+    'FROM ('
+        'SELECT id, author_id, result_score, created_at, '
+        'rank() OVER (PARTITION BY author_id ORDER BY result_score DESC, created_at ASC) AS rank '
+        'FROM challenges_submission '
+        'WHERE challenge_id = %s) as sub '
+    'WHERE sub.rank = 1 '
+    'ORDER BY result_score DESC, created_at ASC'
+)
+
 # Does not pass CI
 # SUBMISSION_SELECT_TOP_SUBMISSION_FOR_CHALLENGE_BY_USER = ('SELECT id, max(result_score) as maxscore '
 #                                                           'FROM challenges_submission '
