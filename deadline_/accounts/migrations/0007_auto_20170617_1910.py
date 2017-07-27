82905086c6213186f110fd5486c177f23a7b6bab
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-07-27 16:29:09 +0300

    [PostgreSQL-DB] Altered migrations, removed some stupid defaults and added ID primary keys in the migration instead of them being added later on

--- deadline_/accounts/migrations/0007_auto_20170617_1910.py
+++ deadline_/accounts/migrations/0007_auto_20170617_1910.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='role',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='accounts.Role'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='accounts.Role'),
         ),
     ]
