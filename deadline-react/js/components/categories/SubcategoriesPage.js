d4044f9cb3eb69242c254758d7b8f8b47f1b52c9
nemalp <lazarov.pl@gmail.com>
2017-08-14 18:35:19 +0300

    Component now fetches data from DB

--- deadline-react/js/components/categories/SubcategoriesPage.js
+++ deadline-react/js/components/categories/SubcategoriesPage.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import Subcategory from './Subcategory'
+import { getCategorySubcategories } from '../../requests'
 
 class SubcategoriesPage extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      subcategories: []
+    }
+
+    this.loadSubcategories = this.loadSubcategories.bind(this)
+    this.loadSubcategories()
+  }
+
+  loadSubcategories () {
+    const categoryId = window.localStorage.categoryId
+
+    getCategorySubcategories(categoryId)
+      .then(subcategories => {
+        this.setState(() => {
+          return {
+            subcategories
+          }
+        })
+      })
+  }
+
   render () {
+    const mainCategory = window.localStorage.categoryName
+
     return (
       <section className='subcategories-page main'>
-        <h1>subcat page</h1>
+        <h1>{mainCategory}</h1>
         <ul className='subcategories'>
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
-          <Subcategory />
+          {this.state.subcategories.map(subcategory => {
+            return <Subcategory key={subcategory.name} name={subcategory.name} />
+          })}
         </ul>
       </section>
     )
