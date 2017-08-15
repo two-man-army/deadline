68592ce03c837d6a3d3a9112e424d614712250d0
nemalp <lazarov.pl@gmail.com>
2017-08-15 12:54:14 +0300

    Add props to Subcategory

--- deadline-react/js/components/categories/SubcategoriesPage.js
+++ deadline-react/js/components/categories/SubcategoriesPage.js
@@ -32,10 +32,15 @@ class SubcategoriesPage extends React.Component {
 
     return (
       <section className='subcategories-page main'>
-        <h1>{mainCategory}</h1>
+        <h1 className='main-category'>{mainCategory}</h1>
         <ul className='subcategories'>
           {this.state.subcategories.map(subcategory => {
-            return <Subcategory key={subcategory.name} name={subcategory.name} />
+            return <Subcategory
+              key={subcategory.name}
+              name={subcategory.name}
+              proficiency={subcategory.proficiency.name}
+              challengeCount={subcategory.challenge_count}
+              solvedChallenges={subcategory.solved_challenges_count} />
           })}
         </ul>
       </section>
