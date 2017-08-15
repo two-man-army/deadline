a5fc884e68bae7eee663866fc85c4cb6d1d0b609
nemalp <lazarov.pl@gmail.com>
2017-08-15 13:23:52 +0300

    Refactor JSX

--- deadline-react/js/components/categories/Subcategory.js
+++ deadline-react/js/components/categories/Subcategory.js
@@ -3,27 +3,39 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import CategoryHeader from './CategoryHeader'
 
-const Subcategory = ({url, name}) => (
-  <li className='subcategory'>
-    <Link to={{pathname: url}}>
-      <section>
-        <CategoryHeader name={name} />
-        <div>
-          <span className='subcat-difficulty'>
-            Difficulty: 3.5/10
-          </span>
-        </div>
-        <div>
-          <span className='subcat-score'>Score: 30</span>
-        </div>
-      </section>
-    </Link>
-  </li>
+const Subcategory = ({
+  url,
+  name,
+  proficiency,
+  solvedChallenges,
+  challengeCount
+  }) => (
+    <li className='subcategory'>
+      <Link to={{pathname: url}}>
+        <section>
+          <CategoryHeader name={name} />
+          <div className='category-info'>
+            <div className='proficiency'>
+              {proficiency}
+            </div>
+            <div className='completed-challenges'>
+              <div className='challenges-progress'>
+                {solvedChallenges}/{challengeCount}
+              </div>
+              <div className='status-text'>Completed</div>
+            </div>
+          </div>
+        </section>
+      </Link>
+    </li>
 )
 
 Subcategory.propTypes = {
   url: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  proficiency: PropTypes.string.isRequired,
+  solvedChallenges: PropTypes.number.isRequired,
+  challengeCount: PropTypes.number.isRequired
 }
 
 export default Subcategory
