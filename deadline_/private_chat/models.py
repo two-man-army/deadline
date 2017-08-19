d54640862661f1c0bff1cb9ae954b351a45eaf67
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-08-19 18:52:43 +0300

    [Chat] Slight refactor on DialogManager.get_or_create_dialog_with_users()

--- deadline_/private_chat/models.py
+++ deadline_/private_chat/models.py
@@ -20,9 +20,13 @@ class DialogManager(models.Manager):
         """
         Gets or creates the dialog between user_owner and user_opponent
         """
-        if not self.filter(Q(owner=user_owner, opponent=user_opponent) | Q(opponent=user_owner, owner=user_opponent)).exists():
-            return self.create(owner=user_owner, opponent=user_opponent)
+        dialog: Dialog = self.fetch_dialog_with_users(user_owner, user_opponent)
+        if dialog is None:
+            dialog = self.create(owner=user_owner, opponent=user_opponent)
 
+        return dialog
+
+    def fetch_dialog_with_users(self, user_owner, user_opponent):
         return self.filter(Q(owner=user_owner, opponent=user_opponent) | Q(opponent=user_owner, owner=user_opponent)).first()
 
 
