d31f564719f11ba638dd7553759ee8c5ad0cfe25
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-08-20 13:55:58 +0300

    [Chat] Changed 'yield from' syntax to def async/await, deleted some unused methods

--- deadline_/private_chat/handlers.py
+++ deadline_/private_chat/handlers.py
@@ -34,126 +34,32 @@ class WebSocketConnection:
         return self.owner_id == other.owner_id and self.opponent_id == other.opponent_id
 
 
-def get_user_from_session(session_key):
-    """
-    Gets the user from current User model using the passed session_key
-    :param session_key: django.contrib.sessions.models.Session - session_key
-    :return: User instance or None if not found
-    """
-    session = Session.objects.get(session_key=session_key)
-    session_data = session.get_decoded()
-    uid = session_data.get('_auth_user_id')
-    user = get_user_model().objects.filter(id=uid).first()  # get object or none
-    return user
-
-
 logger = logging.getLogger('django-private-dialog')
 ws_connections: {(int, int): WebSocketConnection} = {}
 
 
-@asyncio.coroutine
-def send_message(conn, payload):
+async def send_message(conn, payload):
     """
     Distibuted payload (message) to one connection
     """
     try:
-        yield from conn.send(json.dumps(payload))
+        await conn.send(json.dumps(payload))
     except Exception as e:
         logger.debug(f'Could not send message to websocket due to {e}')
 
 
-@asyncio.coroutine
-def fanout_message(connections, payload):
+async def fanout_message(connections, payload):
     """
     distributes payload (message) to all connected ws clients
     """
     for conn in connections:
         try:
-            yield from conn.send(json.dumps(payload))
+            await conn.send(json.dumps(payload))
         except Exception as e:
             logger.debug(f'Could not send message to multiple websockets due to {e}')
 
 
-@asyncio.coroutine
-def gone_online(stream):
-    """
-    Distributes the users online status to everyone he has dialog with
-    """
-    while True:
-        packet = yield from stream.get()
-        session_id = packet.get('session_key')
-        if session_id:
-            user_owner = get_user_from_session(session_id)
-            if user_owner:
-                logger.debug('User '+user_owner.username+' gone online')
-                # find all connections including user_owner as opponent,
-                # send them a message that the user has gone online
-                online_opponents = list(filter(lambda x: x[1] == user_owner.username, ws_connections))
-                online_opponents_sockets = [ws_connections[i] for i in online_opponents]
-                yield from fanout_message(online_opponents_sockets,
-                                          {'type': 'gone-online', 'usernames': [user_owner.username]})
-            else:
-                pass  # invalid session id
-        else:
-            pass  # no session id
-
-
-@asyncio.coroutine
-def check_online(stream):
-    """
-    Used to check user's online opponents and show their online/offline status on page on init
-    """
-    while True:
-        packet = yield from stream.get()
-        session_id = packet.get('session_key')
-        opponent_username = packet.get('username')
-        if session_id and opponent_username:
-            user_owner = get_user_from_session(session_id)
-            if user_owner:
-                # Find all connections including user_owner as opponent
-                online_opponents = list(filter(lambda x: x[1] == user_owner.username, ws_connections))
-                logger.debug('User '+user_owner.username+' has '+str(len(online_opponents))+' opponents online')
-                # Send user online statuses of his opponents
-                socket = ws_connections.get((user_owner.username, opponent_username))
-                if socket:
-                    online_opponents_usernames = [i[0] for i in online_opponents]
-                    yield from send_message(socket,
-                                              {'type': 'gone-online', 'usernames': online_opponents_usernames})
-                else:
-                    pass  # socket for the pair user_owner.username, opponent_username not found
-                    # this can be in case the user has already gone offline
-            else:
-                pass  # invalid session id
-        else:
-            pass  # no session id or opponent username
-
-
-@asyncio.coroutine
-def gone_offline(stream):
-    """
-    Distributes the users online status to everyone he has dialog with
-    """
-    while True:
-        packet = yield from stream.get()
-        session_id = packet.get('session_key')
-        if session_id:
-            user_owner = get_user_from_session(session_id)
-            if user_owner:
-                logger.debug('User '+user_owner.username+' gone offline')
-                # find all connections including user_owner as opponent,
-                #  send them a message that the user has gone offline
-                online_opponents = list(filter(lambda x: x[1] == user_owner.username, ws_connections))
-                online_opponents_sockets = [ws_connections[i] for i in online_opponents]
-                yield from fanout_message(online_opponents_sockets,
-                                          {'type': 'gone-offline', 'username': user_owner.username})
-            else:
-                pass  # invalid session id
-        else:
-            pass  # no session id
-
-
-@asyncio.coroutine
-def fetch_dialog_token(stream):
+async def fetch_dialog_token(stream):
     """
     Returns the Dialog token for a specific conversation and for the specific participant
 
@@ -166,19 +72,19 @@ def fetch_dialog_token(stream):
     }
     """
     while True:
-        packet = yield from stream.get()
+        packet = await stream.get()
         owner_id, opponent_id = int(packet.get('user_id', '-1')), int(packet.get('opponent_id', '-1'))
 
         to_send_message, payload = _fetch_dialog_token(packet, owner_id, opponent_id)
         if to_send_message:
-            yield from send_message(ws_connections[(owner_id, opponent_id)].web_socket, payload)
+            await send_message(ws_connections[(owner_id, opponent_id)].web_socket, payload)
 
             opponent_is_online = (opponent_id, owner_id) in ws_connections and ws_connections[(opponent_id, owner_id)].is_valid
-            yield from send_message(ws_connections[(owner_id, opponent_id)].web_socket,
+            await send_message(ws_connections[(owner_id, opponent_id)].web_socket,
                                     {'type': 'online-check', 'is_online': opponent_is_online})
             # Notify the opponent that we came online
             if opponent_is_online:
-                yield from send_message(ws_connections[(opponent_id, owner_id)].web_socket,
+                await send_message(ws_connections[(opponent_id, owner_id)].web_socket,
                                         {'type': 'online-check', 'is_online': True})
 
 
@@ -205,8 +111,7 @@ def _fetch_dialog_token(packet: dict, owner_id, opponent_id) -> (bool, dict):
     return True, {'conversation_token': token}
 
 
-@asyncio.coroutine
-def new_messages_handler(stream):
+async def new_messages_handler(stream):
     """
     Receives a message from a user and direct it to the recipient
 
@@ -220,7 +125,7 @@ def new_messages_handler(stream):
     }
     """
     while True:
-        packet = yield from stream.get()
+        packet = await stream.get()
         owner_id, opponent_id = packet.get('user_id'), packet.get('opponent_id')
         to_send, is_err, payload = _new_messages_handler(packet, owner_id, opponent_id)
 
@@ -234,7 +139,7 @@ def new_messages_handler(stream):
                 if opponent_socket.is_valid:
                     connections.append(opponent_socket.web_socket)
 
-            yield from fanout_message(connections, payload)
+            await fanout_message(connections, payload)
 
 
 def _new_messages_handler(packet: dict, owner_id, opponent_id):
@@ -279,54 +184,9 @@ def _new_messages_handler(packet: dict, owner_id, opponent_id):
 
     return True, False, payload_to_send
 
-# @asyncio.coroutine
-# def users_changed_handler(stream):
-#     """
-#     Sends connected client list of currently active users in the chatroom
-#     """
-#     while True:
-#         yield from stream.get()
-#
-#         # Get list list of current active users
-#         users = [
-#             {'username': username, 'uuid': uuid_str}
-#             for username, uuid_str in ws_connections.values()
-#         ]
-#
-#         # Make packet with list of new users (sorted by username)
-#         packet = {
-#             'type': 'users-changed',
-#             'value': sorted(users, key=lambda i: i['username'])
-#         }
-#         logger.debug(packet)
-#         yield from fanout_message(ws_connections.keys(), packet)
-
-#
-# @asyncio.coroutine
-# def is_typing_handler(stream):
-#     """
-#     Show message to opponent if user is typing message
-#     """
-#     while True:
-#         packet = yield from stream.get()
-#         session_id = packet.get('session_key')
-#         user_opponent = packet.get('username')
-#         typing = packet.get('typing')
-#         if session_id and user_opponent and typing is not None:
-#             user_owner = get_user_from_session(session_id)
-#             if user_owner:
-#                 opponent_socket = ws_connections.get((user_opponent, user_owner.username))
-#                 if typing and opponent_socket:
-#                     yield from target_message(opponent_socket,
-#                                               {'type': 'opponent-typing', 'username': user_opponent})
-#             else:
-#                 pass  # invalid session id
-#         else:
-#             pass  # no session id or user_opponent or typing
 
 
-@asyncio.coroutine
-def main_handler(websocket, path):
+async def main_handler(websocket, path):
     """
     An Asyncio Task is created for every new websocket client connection
     that is established. This coroutine listens to messages from the connected
@@ -347,18 +207,18 @@ def main_handler(websocket, path):
     if (owner.id, opponent.id) not in ws_connections:
         ws_connections[(owner.id, opponent.id)] = WebSocketConnection(websocket, owner_id, opponent_id)
 
-    yield from send_message(websocket, {'tank': 'YOU ARE CONNECTED :)'})
+    await send_message(websocket, {'tank': 'YOU ARE CONNECTED :)'})
 
     # While the websocket is open, listen for incoming messages/events
     try:
         while websocket.open:
-            data = yield from websocket.recv()
+            data = await websocket.recv()
             if not data:
                 continue
 
             try:
                 logger.debug(f'Data is {data}')
-                yield from router.MessageRouter(data)()
+                await router.MessageRouter(data)()
             except Exception as e:
                 logger.error(f'Could not route message {e}')
 
@@ -367,8 +227,8 @@ def main_handler(websocket, path):
     finally:
         del ws_connections[(owner.id, opponent.id)]
         if (opponent.id, owner.id) in ws_connections and ws_connections[(opponent.id, owner.id)].is_valid:
-            yield from send_message(ws_connections[(opponent.id, owner.id)].web_socket,
-                                    {'type': 'online-check', 'is_online': False})
+            await send_message(ws_connections[(opponent.id, owner.id)].web_socket,
+                               {'type': 'online-check', 'is_online': False})
 
 
 def fetch_and_validate_participants(owner_id: int, opponent_id: int) -> (User, User):
