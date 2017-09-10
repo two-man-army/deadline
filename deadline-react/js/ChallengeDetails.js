fbe8b4a8cad9cfc305d7046839909eb1796c9e03
nemalp <lazarov.pl@gmail.com>
2017-09-10 20:54:35 +0300

    Refactoring - change Tabs component

--- deadline-react/js/ChallengeDetails.js
+++ deadline-react/js/ChallengeDetails.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Tabs, Tab} from 'material-ui/Tabs'
+import { Tabs } from 'antd'
 import {getSelfUserTopSubmission, getTopSolutions, getChallengeDetails, getAllUserSolutions} from './requests.js'
 import ChallengeBoard from './ChallengeBoard.js'
 import SubmissionsTable from './semantic_ui_components/SubmissionsTable.js'
 import LeaderboardTable from './semantic_ui_components/LeaderboardTable.js'
 import { Container } from 'semantic-ui-react'
-import SwipeableViews from 'react-swipeable-views'
 
 class ChallengeDetails extends React.Component {
   constructor (props) {
@@ -86,46 +85,23 @@ class ChallengeDetails extends React.Component {
   }
 
   render () {
-    const styles = {
-      headline: {
-        fontSize: 24,
-        paddingTop: 16,
-        marginBottom: 12,
-        fontWeight: 400
-      },
-      slide: {
-        padding: 10
-      }
-    }
+    const TabPane = Tabs.TabPane
 
     return (
-      <div className='main'>
+      <div className='main challenge-details'>
         <Container>
-          <Tabs
-            onChange={this.handleTabChange}
-            value={this.state.slideIndex}
-            className='challenge-details-tab'
-            tabItemContainerStyle={{background: '#303336'}}
-            inkBarStyle={{color: '#f55333'}}
-          >
-            <Tab label='Challenge' value={0} onClick={() => { this.handleTabChange(0) }} buttonStyle={{fontWeight: 'bold'}} />
-            <Tab label='Submissions' value={1} onClick={() => { this.handleTabChange(1) }} buttonStyle={{fontWeight: 'bold'}} />
-            <Tab label='Leaderboard' value={2} onClick={() => { this.handleTabChange(2) }} buttonStyle={{fontWeight: 'bold'}} />
-          </Tabs>
-          <SwipeableViews
-            index={this.state.slideIndex}
-            onChangeIndex={this.handleTabChange}
-            enableMouseEvents={false}>
-            <div>
+          <h1 className='challenge-name'>{this.state.name}</h1>
+          <Tabs defaultActiveKey='1'>
+            <TabPane tab='CHALLENGE' key='1'>
               <ChallengeBoard {...this.state} userInfo={this.state.userInfo} modifyScore={this.modifyScore} />
-            </div>
-            <div style={styles.slide}>
+            </TabPane>
+            <TabPane tab='SUBMISSIONS' key='2'>
               <SubmissionsTable maxScore={this.state.score} submissions={this.state.solutions} />
-            </div>
-            <div style={styles.slide}>
+            </TabPane>
+            <TabPane tab='LEADERBOARD' key='3'>
               <LeaderboardTable maxScore={this.state.score} submissions={this.state.topSolutions} hasUnlockedSubmissions={this.state.userInfo.maxScore === this.state.score && this.state.score !== undefined} />
-            </div>
-          </SwipeableViews>
+            </TabPane>
+          </Tabs>
         </Container>
       </div>
     )
