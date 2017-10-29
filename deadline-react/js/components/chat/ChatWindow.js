7bdc06ba0049ca66d32ea41e700b70fbdc526799
Ivaylo <ivaylogi@outlook.com>
2017-10-29 03:44:43 +0300

    Add the ChatWindow component and its styles

---
+++ deadline-react/js/components/chat/ChatWindow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChatHeader from './ChatHeader'
+
+const ChatWindow = () => (
+  <section className='chat-window'>
+    <ChatHeader heading='Freind Name' />
+  </section>
+)
+
+export default ChatWindow
