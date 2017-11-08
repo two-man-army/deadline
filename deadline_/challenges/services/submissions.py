c4eb51831393229f08693151222c3a700ea07a2c
Stanislav Kozlovski <stanislav.kozlovski@sumup.com>
2017-11-08 13:42:27 +0200

    [Profile] Added a ceiling on submissions_count_by_date_from_user_since(), stopping it from fetchng submissions older than a year

--- deadline_/challenges/services/submissions.py
+++ deadline_/challenges/services/submissions.py
@@ -1,15 +1,18 @@
 """ Domain code that is not dependant on the underlying ORM/DB/Framework """
+from helpers import get_date_day_difference, datetime_now
 from challenges.models import Submission
 
 
 def submissions_count_by_date_from_user_since(user, since_date) -> dict:
     """
-    :return: A dict, counting the number of submissions the user has made for that date
+    :return: A dict, counting the number of submissions the user has made for each date
      {
         datetime(2017, 10, 10): 15,
         datetime(2017, 9, 10): 12,
      }
     """
+    if get_date_day_difference(datetime_now(), since_date) > 365:
+        raise ValueError(f'Querying submissions count per day for more than a year is not supported!')
     submission_data = Submission.fetch_submissions_count_by_day_from_user_since(user, since_date)
     count_data = {}
     for subm_data in submission_data:
--- deadline_/challenges/tests/test_submission_service.py
+++ deadline_/challenges/tests/test_submission_service.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
-from datetime import datetime
+from datetime import datetime, date
 
 from challenges.services.submissions import submissions_count_by_date_from_user_since
 
 
 class SubmissionServiceTests(TestCase):
-    @patch('challenges.services.submissions.Submission.fetch_submissions_count_from_user_since')
-    def test_submissions_count_by_date_for_user_since_maps_as_expected_and_ignores_empty_count(self, fetch_mock):
+    @patch('challenges.services.submissions.get_date_day_difference')
+    @patch('challenges.services.submissions.Submission.fetch_submissions_count_by_day_from_user_since')
+    def test_submissions_count_by_date_for_user_since_maps_as_expected_and_ignores_empty_count(self, fetch_mock, day_diff_mock):
+        day_diff_mock.return_value = 10
         date_one, date_two, date_three = datetime(2017, 10, 12), datetime(2017, 10, 13), datetime(2012, 10, 12)
         fetch_mock.return_value = [
             {'created_at': date_one, 'count': 15},
@@ -24,3 +26,10 @@ class SubmissionServiceTests(TestCase):
 
         fetch_mock.assert_called_once_with(user_mock, date_one)
         self.assertEqual(expected_data, received_data)
+
+    @patch('challenges.services.submissions.get_date_day_difference')
+    def test_submissions_count_by_date_for_user_since_raises_error_if_since_data_too_early(self, day_diff_mock):
+        day_diff_mock.return_value = 390
+        with self.assertRaises(ValueError):
+            submissions_count_by_date_from_user_since(MagicMock(), MagicMock())
+
