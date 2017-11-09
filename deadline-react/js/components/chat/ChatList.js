ccc59f4c1e43806fad21546ea0150d296668a2cc
Ivaylo <ivaylogi@outlook.com>
2017-11-09 16:05:12 +0200

    Refactoring component structure

--- deadline-react/js/components/chat/ChatList.js
+++ deadline-react/js/components/chat/ChatList.js
@@ -7,8 +7,10 @@ const ChatList = () => (
   <section className='chat-list'>
     <ChatHeader heading='Messages' />
     <Search />
-    <ChatThreadListRow name='Ivaelo Eleev' path='assets/img/pikachu.svg' msg='Hello hows a going mate are you...' />
-    <ChatThreadListRow name='Cesar Vergas' path='assets/img/pikachu.svg' msg='Yesterday I was having a party...' />
+    <ul className='ThreadListContainer'>
+      <ChatThreadListRow name='Ivaelo Eleev' path='assets/img/pikachu.svg' msg='Hello hows a going mate are you...' time='21:12' />
+      <ChatThreadListRow name='Cesar Vergas' path='assets/img/pikachu.svg' msg='Yesterday I was having a party...' time='10:43' />
+    </ul>
   </section>
 )
 
