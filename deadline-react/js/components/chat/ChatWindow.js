e088747fe72fe44f5aea2b3490576a2edc1559a7
Ivaylo <ivaylogi@outlook.com>
2017-11-09 16:08:03 +0200

    Add static components

--- deadline-react/js/components/chat/ChatWindow.js
+++ deadline-react/js/components/chat/ChatWindow.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ChatHeader from './ChatHeader'
+import ChatFriendMsg from './ChatFriendMsg'
+import ChatMyMsg from './ChatMyMsg'
 
 const ChatWindow = () => (
   <section className='chat-window'>
     <ChatHeader heading='Freind Name' />
+    <ChatFriendMsg date='12/05/1993' path='assets/img/pikachu.svg' msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+    <ChatMyMsg msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+    <ChatFriendMsg date='12/05/1993' path='assets/img/pikachu.svg' msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+    <ChatMyMsg msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+    <ChatMyMsg msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit.' />
+    <ChatFriendMsg date='12/05/1993' path='assets/img/pikachu.svg' msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit' />
   </section>
 )
 
