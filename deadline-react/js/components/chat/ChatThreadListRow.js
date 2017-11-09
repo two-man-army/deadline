b4f13c59bb5bbd813d7897b61b58d82d89246792
Ivaylo <ivaylogi@outlook.com>
2017-11-09 16:06:22 +0200

    Refactoring ChatThreadListRow component

--- deadline-react/js/components/chat/ChatThreadListRow.js
+++ deadline-react/js/components/chat/ChatThreadListRow.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import UserImage from '../UserImage'
 import Username from '../Username'
+import ChatTimestamp from './ChatTimestamp'
+import ChatUserMsg from './ChatUserMsg'
 import PropTypes from 'prop-types'
 
-const ChatThreadListRow = ({name, path, msg}) => (
-  <ul className='chat-member'>
-    <li className='chat-member-list'>
-      <UserImage path={path} />
-      <div className='chat-member-wrapper'>
+const ChatThreadListRow = ({name, path, msg, time}) => (
+  <li className='chat-threadlist '>
+    <UserImage path={path} />
+    <div className='chat-member-wrapper'>
+      <div className='chat-member-title'>
         <Username name={name} />
-        <p>{msg}</p>
+        <ChatTimestamp time={time} />
       </div>
-    </li>
-  </ul>
+      <ChatUserMsg msg={msg} />
+    </div>
+  </li>
 )
 
 ChatThreadListRow.propTypes = {
   name: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
-  msg: PropTypes.string.isRequired
+  msg: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired
 }
 
 export default ChatThreadListRow
