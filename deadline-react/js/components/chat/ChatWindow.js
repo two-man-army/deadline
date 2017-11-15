40fba2db849db1d88ad55d69d6ecc858c9271c61
Ivaylo <ivaylogi@outlook.com>
2017-11-15 10:46:32 +0200

    Add the ChatMsgBox to ChatWindow

--- deadline-react/js/components/chat/ChatWindow.js
+++ deadline-react/js/components/chat/ChatWindow.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ChatHeader from './ChatHeader'
 import ChatFriendMsg from './ChatFriendMsg'
 import ChatMyMsg from './ChatMyMsg'
+import ChatMsgBox from './ChatMsgBox'
 
 const ChatWindow = () => (
   <section className='chat-window'>
@@ -12,6 +13,7 @@ const ChatWindow = () => (
     <ChatMyMsg msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
     <ChatMyMsg msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit. consectetur adipiscing elit.' />
     <ChatFriendMsg date='12/05/1993' path='assets/img/pikachu.svg' msg='Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit' />
+    <ChatMsgBox />
   </section>
 )
 
